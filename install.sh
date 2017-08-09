36fa572404077d225ec2d8de5a5eb6e8514223d3
David Hatch <hozer48@gmail.com>
2017-08-09 13:36:32 -0700

    Install - Fixes symlinking not working.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Master install script for symlinking and vim setup.
 
-make_symlinks.sh
+./make_symlinks.sh
 
 # Install vim plugin manager - Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
