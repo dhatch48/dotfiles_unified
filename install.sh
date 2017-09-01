d9559e36d8d0d0ea9f28fbad09d15389c5dc21df
David Hatch <hozer48@gmail.com>
2017-09-01 15:41:28 -0700

    install, make_symlinks - Adds installing TPM - tmux plugin manager and tmux-yank plugin.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Master install script for symlinking and vim setup.
 
+# Make dir for TPM
+mkdir ~/dotfiles/tmux
+
 ./make_symlinks.sh
+
+# Install TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install vim plugin manager - Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
